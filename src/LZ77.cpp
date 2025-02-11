fbc28ed8494320e80e915b9485db5e71e36056fb
Zachary Frederick <zachpack200@gmail.com>
2025-02-11 17:33:54 -0500

    completed the LZ77 algorithm

--- src/LZ77.cpp
+++ src/LZ77.cpp
@@ -1,76 +1,215 @@
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
 
-
-
-struct Token{
-    std::byte length;
+struct Token
+{
+    char length;
     char distance;
     char character;
+
+    Token(u_int8_t _length, u_int8_t _distance, char _character)
+    {
+        length = _length;
+        distance = _distance;
+        character = _character;
+    }
+
+    Token() {}
 };
 
-class LZ77{
+class LZ77
+{
 
-    private:
+private:
+#define BUFFER_LENGTH 256
+#define COMPARE_WINDOW 256
 
-    std::string text;
+    std::vector<char> text;
     std::vector<Token> tokenText;
-    std::string compressedText;
+    std::vector<char> compressedText;
 
+    void createTokenArray()
+    {
 
-    void createTokenArray(){
-        
-    }
-
-    void tokenArrayToString(){
-        
-        std::vector<char> buildText;
-
-        for (auto && token : tokenText)
+        for (size_t i = 0; i < text.size();)
         {
-            buildText.emplace_back(token.length);
-            buildText.emplace_back(token.distance);
-            buildText.emplace_back(token.character);
+            u_int8_t iderateAmount = 0;
+
+            int startWindow = 0 < (i - BUFFER_LENGTH) ? 0 : i - BUFFER_LENGTH; // max(0, i-BUFFER_LENGTH)
+            u_int8_t largestMatch = 0;
+
+            for (size_t j = startWindow; j < i && i - j > largestMatch; j++)
+            {
+                int matchLength = 0;
+
+                while (text[j + matchLength] == text[i + matchLength])
+                {
+                    matchLength++;
+                }
+
+                if (matchLength > largestMatch)
+                {
+                    largestMatch = matchLength;
+                    iderateAmount = i - j;
+                }
+            }
+            std::cout << "<" << (int)iderateAmount << ", " << (int)largestMatch << ", " << text[i + largestMatch] << ">" << std::endl;
+            tokenText.emplace_back(Token(iderateAmount, largestMatch, text[i + largestMatch]));
+
+            i = i + largestMatch + 1;
         }
-        
-        compressedText = std::string (buildText.begin(), buildText.end());
     }
 
-
-    public:
-
-    void compress(){
-
+    void tokenArrayToString()
+    {
+        for (auto &&token : tokenText)
+        {
+            compressedText.emplace_back((char)token.length);
+            compressedText.emplace_back((char)token.distance);
+            compressedText.emplace_back(token.character);
+        }
     }
 
-    void decompress(){
+    void stringToTokenArray()
+    {
+        for (size_t i = 0; i < compressedText.size(); i = i + 3)
+        {
+            Token temp;
+            temp.length = compressedText[i];
+            temp.distance = compressedText[i + 1];
+            temp.character = compressedText[i + 2];
 
+            tokenText.emplace_back(temp);
+
+            // std::cout << "<" << (int)temp.length << ", " << (int)temp.distance << ", " << temp.character << ">\t";
+        }
     }
 
-    std::string getText(){
-        return text;
+public:
+    LZ77()
+    {
     }
 
-    std::vector<Token>& getTokenText(){
-        return tokenText;
+    std::string compress(std::string _text)
+    {
+        if (_text.empty())
+        {
+            // check is compression is avaliable
+            return NULL;
+        }
+        resetInfo();
+
+        text = std::vector(_text.begin(), _text.end());
+
+        for (size_t i = 0; i < text.size();)
+        {
+            u_int8_t iderateAmount = 0;
+
+            int startWindow = 0 < (i - BUFFER_LENGTH) ? 0 : i - BUFFER_LENGTH; // max(0, i-BUFFER_LENGTH)
+            u_int8_t largestMatch = 0;
+
+            for (size_t j = startWindow; j < i && i - j > largestMatch; j++)
+            {
+                u_int8_t matchLength = 0;
+
+                while (text[j + matchLength] == text[i + matchLength])
+                {
+                    matchLength++;
+                }
+
+                if (matchLength > largestMatch)
+                {
+                    largestMatch = matchLength;
+                    iderateAmount = i - j;
+                }
+            }
+            // std::cout << "<" << (int)iderateAmount << ", " << (int)largestMatch << ", " << _text[i + largestMatch] << ">" << std::endl;
+            tokenText.emplace_back(Token(iderateAmount, largestMatch, _text[i + largestMatch]));
+
+            i = i + largestMatch + 1;
+        }
+
+        tokenArrayToString();
+
+        return std::string(compressedText.begin(), compressedText.end());
     }
 
-    std::string getCompressedText(){
-        return compressedText;
+    std::string decompress(std::string _compressedText)
+    {
+        if (_compressedText.empty())
+        {
+            return NULL;
+        }
+        resetInfo();
+
+        compressedText = std::vector<char>(_compressedText.begin(), _compressedText.end());
+
+        stringToTokenArray();
+
+        for (size_t i = 0; i < tokenText.size(); i++)
+        {
+            char length = tokenText[i].length;
+            char distance = tokenText[i].distance;
+
+            long startingPosition = text.size() - length;
+
+            for (size_t j = startingPosition; j < startingPosition + distance; j++)
+            {
+                text.emplace_back(text[j]);
+                // std::cout << "(" << i << ", " << j << ")\tcopy:\t" << text[j] << std::endl;
+            }
+
+            text.emplace_back(tokenText[i].character);
+            // std::cout << "(" << i << ")\tnew:\t" << tokenText[i].character << std::endl;
+        }
+
+        return std::string(text.begin(), text.end());
     }
 
-    std::string setText(std::string _text){
-        text = _text;
+    void resetInfo()
+    {
+        text.clear();
+        tokenText.clear();
+        compressedText.clear();
     }
-
-    std::string setTokenText(std::vector<Token>& _tokenText){
-        tokenText = _tokenText;
-    }
-
-    std::string setCompressedText(std::string _compressedText){
-        compressedText = _compressedText;
-    }
-
 };
+
+int main()
+{
+
+    std::ifstream f("/home/zfuser/myucf/COP4520/parallelize-deflate/input.txt");
+
+    // Check if the file is successfully opened
+    if (!f.is_open())
+    {
+        std::cout << "Error opening the file!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string test = "";
+
+    std::getline(f, test);
+    // while (std::getline(f, test)){}
+        // std::cout << test << std::endl;
+
+    f.close();
+
+    // std::cout << "Original: " << test << std::endl;
+    std::cout << "Original length: " << test.length() << std::endl;
+
+    // LZ77 compression algorithm
+    LZ77 compressor;
+    std::string LZ_compressed = compressor.compress(test);
+
+    // std::cout << "Compressed: " << LZ_compressed << std::endl;
+    std::cout << "Compressed length: " << LZ_compressed.length() << std::endl;
+
+    std::string LZ_decompressed = compressor.decompress(LZ_compressed);
+    // std::cout << "Decompressed: " << LZ_decompressed << std::endl;
+
+    std::cout << "Origonal == Decompressed: " << (test.compare(LZ_decompressed) == 0) << std::endl;
+}
