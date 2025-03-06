d5e54cae63686e53e21d05ec01d48021e9a83751
Tran Pham <tranpham9@yahoo.com>
2025-03-06 16:25:43 -0500

    canonical huffman method

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 #define COMPRESSED_FILE_EXTENTION "pdc"
 using namespace std;
 
-void handleFile(std::string fileName) {
-    std::string fileData = readFile(fileName);
+void handleFile(string fileName) {
+    string fileData = readFile(fileName);
 
     LZ77 lz77;
     if (!getFileExtension(fileName).compare(COMPRESSED_FILE_EXTENTION)) {
@@ -19,18 +19,18 @@ void handleFile(std::string fileName) {
         /*
             Huffman decompression
         */
-        std::string huffman_decompress = fileData;
+        string huffman_decompress = fileData;
 
-        std::string final_decompressed_text = lz77.decompress(fileData);
+        string final_decompressed_text = lz77.decompress(fileData);
 
-        std::string name = fileName.substr(0, fileName.size() - 4);
+        string name = fileName.substr(0, fileName.size() - 4);
 
         writeFile(final_decompressed_text, name);
     } else {
         // compress
         cout << fileName << ": compression" << endl;
 
-        std::string lz_compressed = lz77.compress(fileData);
+        string lz_compressed = lz77.compress(fileData);
 
         /*
             Huffman Compress
@@ -50,14 +50,14 @@ void handleFile(std::string fileName) {
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
-        std::cout << "no input paramater was inputed" << std::endl;
+        cout << "no input paramater was inputed" << endl;
         return EXIT_FAILURE;
     }
 
     vector<thread> threads;
 
     for (size_t i = 1; i < argc; i++) {
-        std::string fileName = argv[i];
+        string fileName = argv[i];
         threads.emplace_back(handleFile, fileName);
         // handleFile(fileName);
     }
